76fb7844aa1033a2215abbcbc78a410aff913b93
kataai <kataai@Aurora.lan>
2018-11-27 03:22:10 +0100

    Added comments for clarity

--- bot_actions/__init__.py
+++ bot_actions/__init__.py
@@ -1,5 +1,4 @@
 '''File for functions, that implements bot functions'''
-import re
 
 from discord import Server
 from . import actions
@@ -20,9 +19,12 @@ class MessageHandler:
         '''Method that parse command and returns corresponding method'''
         print('Message:', message.content)
         if self.server:
-            self.messageEmojiTester(message)
+            """The branch that gets called when there is no command character at the start of a message.
+               Use for tasks that have to check every message."""
+            #self.messageEmojiTester(message)
 
         if self.server and message.content.startswith(self.command_character):
+            """The branche that gets called when there is a command character at the start."""
             # todo: return different actions in different cases
             # todo: parse for amount of days. Some other time structure?
             return actions.EmojiCounter(self.server.channels, 
@@ -30,10 +32,10 @@ class MessageHandler:
 
         return actions.ActionInterface()  # todo: or None?
 
-    def messageEmojiTester(self, message):
-        """Looks for custom emoji, and prints some info about it to the command line"""
-        messageEmojiPattern = re.compile("(<:?[a-zA-Z]+:?[0-9]+>)")
-        result=messageEmojiPattern.findall(message.content)
-        if result:
-            for r in result:
-                print("emoji found: {}, sent in server: {}, in channel: {}, by: {}".format(r,message.server, message.channel,message.author))
+#     def messageEmojiTester(self, message):
+#         """Looks for custom emoji, and prints some info about it to the command line"""
+#         messageEmojiPattern = re.compile("(<:?[a-zA-Z]+:?[0-9]+>)")
+#         result=messageEmojiPattern.findall(message.content)
+#         if result:
+#             for r in result:
+#                 print("emoji found: {}, sent in server: {}, in channel: {}, by: {}".format(r,message.server, message.channel,message.author))
